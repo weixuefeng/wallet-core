f278bf90b35101402fdf28fb52f3bb664ed58bf0
Pony <weixuefeng1018@gmail.com>
2023-06-19 11:24:10 +0800

    feat: build wasm for browser extension, remove feature for fs-storage

--- wasm/src/keystore/fs-storage.ts
+++ wasm/src/keystore/fs-storage.ts
@@ -5,41 +5,30 @@
 // file LICENSE at the root of the source code distribution tree.
 
 import * as Types from "./types";
-import * as fs from "fs/promises";
+// import * as fs from "fs/promises";
 
 // FileSystem Storage
 export class FileSystemStorage implements Types.IStorage {
-  private readonly directory: string;
 
   constructor(directory: string) {
-    this.directory = directory.endsWith("/") ? directory : directory + "/";
+    // this.directory = directory.endsWith("/") ? directory : directory + "/";
   }
 
   getFilename(id): string {
-    return this.directory + id + ".json";
-  }
+    throw Types.Error.WalletNotFound;  }
 
   get(id: string): Promise<Types.Wallet> {
-    return fs
-      .readFile(this.getFilename(id))
-      .then((data) => JSON.parse(data.toString()) as Types.Wallet);
+    throw Types.Error.WalletNotFound;  
   }
 
   set(id: string, wallet: Types.Wallet): Promise<void> {
-    return fs.writeFile(this.getFilename(id), JSON.stringify(wallet));
-  }
+    throw Types.Error.WalletNotFound;  }
 
   loadAll(): Promise<Types.Wallet[]> {
-    return fs.readdir(this.directory).then((files) => {
-      return Promise.all(
-        files
-          .filter((file) => file.endsWith(".json"))
-          .map((file) => this.get(file.replace(".json", "")))
-      );
-    });
+    throw Types.Error.WalletNotFound;
   }
 
   delete(id: string, password: string): Promise<void> {
-    return fs.unlink(this.getFilename(id));
+    throw Types.Error.WalletNotFound;
   }
 }
