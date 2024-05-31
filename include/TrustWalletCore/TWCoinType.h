bcc2a5a59f79aeba2fabf3318e38f3e6f8a30433
gtmickey <gtmickey439@gmail.com>
2024-05-31 14:54:57 +0800

    Add sonic coin

--- include/TrustWalletCore/TWCoinType.h
+++ include/TrustWalletCore/TWCoinType.h
@@ -190,6 +190,7 @@ enum TWCoinType {
     TWCoinTypeVara = 913,
     TWCoinTypeAvail = 709,
     TWCoinTypeNibiru = 23000118,
+    TWCoinTypeSonic = 10000501,
     // end_of_tw_coin_type_marker_do_not_modify
 };
 
