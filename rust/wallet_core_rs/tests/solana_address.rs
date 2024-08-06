c4cfaa5a0662c3713b7f4a9e22192e088a7bc973
pony <weixuefeng1018@gmail.com>
2024-08-06 16:38:39 +0800

    feat: update artifactId for android build & solana address test

--- rust/wallet_core_rs/tests/solana_address.rs
+++ rust/wallet_core_rs/tests/solana_address.rs
@@ -25,3 +25,24 @@ fn test_solana_address_default_token_address() {
 
     assert_eq!(actual, "EDNd1ycsydWYwVmrYZvqYazFqwk1QjBgAUKFjBoz1jKP");
 }
+
+#[test]
+fn test_solana_address_default_token_2022_address() {
+    let main_address = "5RMMQUsbkeHE6PsNjBZ3Cnoo1yehgwSnJjp1abGroDkL";
+    let main_address = TWStringHelper::create(main_address);
+
+    let token_mint_address = "BSQCmMAFB9itonyVSLsUxX92Ne1rgBZFqothBk3q91k6";
+    let token_mint_address = TWStringHelper::create(token_mint_address);
+
+    let actual = unsafe {
+        TWStringHelper::wrap(tw_solana_address_default_token_address(
+            main_address.ptr(),
+            token_mint_address.ptr(),
+            true
+        ))
+    }
+    .to_string()
+    .expect("!tw_solana_address_default_token_address returned a nullptr");
+
+    assert_eq!(actual, "8ZZWsB6HjfeRyoTrobToJh5NA6baKYy9wxwoKQEjYLaM");
+}
